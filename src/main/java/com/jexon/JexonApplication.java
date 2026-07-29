a8faea073e59d2c5f877ff969143575ebac46049
Jmin0613 <jumin1356@gmail.com>
2026-07-29 15:10:09 +0900

    chore: initialize Jexon project

---
+++ src/main/java/com/jexon/JexonApplication.java
@@ -0,0 +1,13 @@
+package com.jexon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JexonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JexonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jexon/JexonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jexon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JexonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
